c54382936efcaa37119c2eea73be5c7fbdc3e80c
KirstenZimbardi <kirsten.zimbardi@gmail.com>
2016-04-12 13:44:24 +1000

    Put for loop back in nnCostFunction
    Not sure what I had done - seemed to have reverted nnCostFx all the way
    back to before the for loop? Or maybe it never went in from sandpit?
    Confused!!!

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -1,4 +1,4 @@
-function [J grad] = nnCostFunction(nn_params, ...
+function [cost grad] = nnCostFunction(nn_params, ...
                                    input_layer_size, ...
                                    hidden_layer_size, ...
                                    num_labels, ...
@@ -17,37 +17,62 @@ function [J grad] = nnCostFunction(nn_params, ...
 % Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
 % for our 2 layer neural network
 
-Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
-                 hidden_layer_size, (input_layer_size + 1));
+%Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+%                 hidden_layer_size, (input_layer_size + 1));
 
-Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
-                 num_labels, (hidden_layer_size + 1));
+%Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+%                 num_labels, (hidden_layer_size + 1));
+
+% random initialisation
+initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
 
             
+% adding X0            
 [m, n] = size(X);
-
-% Add ones to the X data matrix
 X = [ones(m, 1) X];
 [m, n] = size(X);
-            
-% Setup some useful variables
-%m = size(X, 1);
 
-%% g function
-% layer 1 -> 2
-z = X * Theta1';
-a2 = sigmoid(z);
+% for loop
 
-%% g function
-% layer 2 ->3
-a2 = [ones(m, 1) a2];
-z2 = a2 * Theta2';
-h = sigmoid(z2);
+iT1t = initial_Theta1';
+iT2t = initial_Theta2';
+%iT1t = Theta1';
+%iT2t = Theta2';
+a2 = ones(m,hidden_layer_size+1);
+
+for k = 1:num_labels
+    c(:,k) = y == k;
+end
+
+for t = 1:m
+    %forward layer 1 -> 2
+    z(t,:) = X(t,:) * iT1t;
+    %forward layer 2 -> 3
+    a2(t,2:end) = sigmoid(z(t,:));
+    z2(t,:) = a2(t,:) * iT2t;
+    h(t,:) = sigmoid(z2(t,:));
+    %back prop layer 3 ->
+    d3(t,:) = h(t,:) - c(t,:);
+    d2(t,:) = h(t,:) .* (1-h(t,:)); %old version
+end
+
+%gradient
+Theta2_grad = d3' * a2;
+a2g = a2(:,2:end);
+Theta1_grad = a2g' * X;
+grad = [Theta1_grad(:) ; Theta2_grad(:)];
+
+cost = (1/m) * sum(grad);
+
+%% 
+
          
 % You need to return the following variables correctly 
-J = 0;
-Theta1_grad = zeros(size(Theta1));
-Theta2_grad = zeros(size(Theta2));
+%J = 0;
+%Theta1_grad = zeros(size(Theta1));
+%Theta2_grad = zeros(size(Theta2));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the code by working through the
@@ -81,64 +106,6 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-
-
-%% Cost
-
-for k = 1:num_labels
-    c = y == k;
-    E(k,:) = (-c .* log(h(:,k))) - ((1-c) .* log(1-h(:,k)));
-end
-
-%% sum for 1:k classes
-Ek = sum(E);
-
-%% sum for 1:m training examples
-Em = sum(Ek,2);
-
-%%  * 1/m
-J_unreg = (1/m) * Em;
-
-%% adding regularisation
-%Theta1
-
-Theta1_squared = Theta1(:,2:end) .* Theta1(:,2:end);
-Theta1_SS = sum(Theta1_squared);
-Theta1_SS = sum(Theta1_SS,2);
-
-Theta2_squared = Theta2(:,2:end) .* Theta2(:,2:end);
-Theta2_SS = sum(Theta2_squared);
-Theta2_SS = sum(Theta2_SS,2);
-
-J = J_unreg + ((lambda/(2*m)) * (Theta1_SS + Theta2_SS));
-
-
-%% gradient
-for k = 1:num_labels
-    c(:,k) = y == k;
-end
-%% error for layer 3
-
-d3 = h - c;
-%% error for layer 2
-
-d2 = h .* (1-h);
-% no error for layer 1 since that is the data input
-%% getting Theta1_grad and Theta2_grad based on clued size
-
-Theta2_grad = d3' * a2;
-
-a2g = a2(:,2:(hidden_layer_size+1));
-
-Theta1_grad = a2g' * X;
-
-
-% -------------------------------------------------------------
-
-% =========================================================================
-
-% Unroll gradients
-grad = [Theta1_grad(:) ; Theta2_grad(:)];
 
 
 end
