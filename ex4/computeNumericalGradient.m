218e9e87b337a33fa7a953b187d790cb505f25d0
KirstenZimbardi <kirsten.zimbardi@gmail.com>
2016-04-14 14:24:58 +1000

    Nope - grad stil << numgrad

--- ex4/computeNumericalGradient.m
+++ ex4/computeNumericalGradient.m
@@ -13,9 +13,6 @@ function numgrad = computeNumericalGradient(J, theta)
 %        to theta(i).)
 %                
 
-%theta = nn_params;
-%J = costFunc;
-
 numgrad = zeros(size(theta));
 perturb = zeros(size(theta));
 e = 1e-4;
