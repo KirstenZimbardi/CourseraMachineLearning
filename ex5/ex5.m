641ff3eacfcb9a0dcce992884c48ab400adf262e
KirstenZimbardi <kirsten.zimbardi@gmail.com>
2016-04-18 18:48:49 +1000

    Passed J and grad functions
    but had tried to do learning curve as well and that one is 0/20 so some
    issue still in calc J or grad for validation set…

--- ex5/ex5.m
+++ ex5/ex5.m
@@ -48,7 +48,8 @@ ylabel('Water flowing out of the dam (y)');
 %
 
 theta = [1 ; 1];
-J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+%J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+J = linearRegCostFunction(X, y, theta, 1);
 
 fprintf(['Cost at theta = [1 ; 1]: %f '...
          '\n(this value should be about 303.993192)\n'], J);
@@ -62,14 +63,16 @@ fprintf(['Cost at theta = [1 ; 1]: %f '...
 %
 
 theta = [1 ; 1];
-[J, grad] = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+%[J, grad] = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+[J, grad] = linearRegCostFunction(X, y, theta, 1);
+
 
 fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
          '\n(this value should be about [-15.303016; 598.250744])\n'], ...
          grad(1), grad(2));
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 
 %% =========== Part 4: Train Linear Regression =============
@@ -83,7 +86,9 @@ pause;
 
 %  Train linear regression with lambda = 0
 lambda = 0;
-[theta] = trainLinearReg([ones(m, 1) X], y, lambda);
+%[theta] = trainLinearReg([ones(m, 1) X], y, lambda);
+[theta] = trainLinearReg(X, y, lambda);
+
 
 %  Plot fit over the data
 plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
@@ -93,8 +98,8 @@ hold on;
 plot(X, [ones(m, 1) X]*theta, '--', 'LineWidth', 2)
 hold off;
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 
 %% =========== Part 5: Learning Curve for Linear Regression =============
@@ -109,6 +114,12 @@ lambda = 0;
     learningCurve([ones(m, 1) X], y, ...
                   [ones(size(Xval, 1), 1) Xval], yval, ...
                   lambda);
+              
+%[error_train, error_val] = ...
+%    learningCurve(X, y, ...
+%                  Xval, yval, ...
+%                  lambda);
+              
 
 plot(1:m, error_train, 1:m, error_val);
 title('Learning curve for linear regression')
@@ -122,8 +133,8 @@ for i = 1:m
     fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
 end
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% =========== Part 6: Feature Mapping for Polynomial Regression =============
 %  One solution to this is to use polynomial regression. You should now
